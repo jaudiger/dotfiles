b7a68aabc73af8fcc8f7afa93d3761e9add81578
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-09-16 08:46:49 +0200

    chore(ghostty): update configuration
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/program/ghostty.nix
+++ profiles/program/ghostty.nix
@@ -12,8 +12,10 @@
         font-family = "JetBrainsMono Nerd Font";
         font-size = 11.5;
 
-        selection-invert-fg-bg = true;
-        cursor-invert-fg-bg = true;
+        selection-foreground = "cell-background";
+        selection-background = "cell-foreground";
+        cursor-color = "cell-foreground";
+        cursor-text = "cell-background";
         minimum-contrast = 1.05;
         mouse-hide-while-typing = true;
 
@@ -21,6 +23,9 @@
         background-blur = true;
 
         resize-overlay = "never";
+
+        quick-terminal-size = "25%,75%";
+        quick-terminal-position = "center";
 
         auto-update = "off";
       };
