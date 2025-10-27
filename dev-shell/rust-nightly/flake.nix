0f393771eaf57ab8dbaac75da39199f2ef716dc6
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-10-27 09:47:05 +0100

    feat: update Rust nightly version to 2025-10-26
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- dev-shell/rust-nightly/flake.nix
+++ dev-shell/rust-nightly/flake.nix
@@ -42,7 +42,7 @@
         {
           default = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
-              (rust-bin.nightly."2025-09-18".minimal.override {
+              (rust-bin.nightly."2025-10-26".minimal.override {
                 extensions = [
                   "rust-src"
                   "clippy"
