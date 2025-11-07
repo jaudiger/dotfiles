a70a8aebf178df1ff722d3a2370f3716d56cd56d
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-11-07 13:12:30 +0100

    Udoc(go): update Go package authentication URL to remove a source query
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/development/go.nix
+++ profiles/development/go.nix
@@ -14,7 +14,7 @@ let
 in
 {
   # Per GitLab documentation, this is required in order to clone private repositories using Go tools:
-  # https://docs.gitlab.com/user/project/use_project_as_go_package/?utm_source=chatgpt.com#authenticate-go-requests-to-private-projects
+  # https://docs.gitlab.com/user/project/use_project_as_go_package/?#authenticate-go-requests-to-private-projects
   sops = {
     # To decrypt the secret: "nix-shell -p sops --run 'sops -d secrets/.netrc'"
     # To encrypt the secret: "nix-shell -p sops --run 'sops -e secrets/.netrc' > secrets/.netrc"
