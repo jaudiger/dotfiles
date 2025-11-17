802d2cd1015247a69db5816926f476d9ad82cf0b
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-11-17 21:49:37 +0100

    feat(gemini-cli): add configuration
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/program/gemini-cli.nix
+++ profiles/program/gemini-cli.nix
@@ -5,6 +5,85 @@
     home-manager = {
       programs.gemini-cli = {
         enable = true;
+
+        defaultModel = "gemini-2.5-pro";
+
+        settings = {
+          experimental = {
+            codebaseInvestigatorSettings = {
+              maxNumTurns = 20;
+              maxTimeMinutes = 5;
+              thinkingBudget = 16384;
+            };
+          };
+
+          general = {
+            checkpointing = {
+              enabled = true;
+            };
+            disableAutoUpdate = true;
+            disableUpdateNag = true;
+            enablePromptCompletion = true;
+            preferredEditor = "hx";
+            sessionRetention = {
+              enabled = true;
+              maxAge = "1w";
+            };
+          };
+
+          ide = {
+            hasSeenNudge = true;
+          };
+
+          mcpServers = {
+            context7 = {
+              command = "npx";
+              args = [
+                "-y"
+                "@upstash/context7-mcp"
+              ];
+              trust = true;
+            };
+            github = {
+              command = "docker";
+              args = [
+                "run"
+                "-i"
+                "--rm"
+                "-e"
+                "GITHUB_PERSONAL_ACCESS_TOKEN"
+                "ghcr.io/github/github-mcp-server"
+              ];
+              env = {
+                GITHUB_PERSONAL_ACCESS_TOKEN = "$GITHUB_MCP_PAT";
+              };
+              trust = false;
+            };
+          };
+
+          privacy = {
+            usageStatisticsEnabled = false;
+          };
+
+          security = {
+            auth = {
+              selectedType = "oauth-personal";
+            };
+          };
+
+          tools = {
+            autoAccept = true;
+            shell = {
+              pager = "less";
+              showColor = true;
+            };
+          };
+
+          ui = {
+            showCitations = true;
+            showLineNumbers = true;
+          };
+        };
       };
     };
   };
