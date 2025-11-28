c2372f66597ed03e5ed89607fef62679abaf1d9a
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-11-28 08:13:56 +0100

    chore(rust): add management of cargo config file
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/development/rust.nix
+++ profiles/development/rust.nix
@@ -11,38 +11,50 @@ in
 {
   nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
 
-  modules.home-manager.home = {
-    packages = with pkgs; [
-      (rust-bin.stable.latest.minimal.override {
-        extensions = [
-          "clippy"
-          "rustfmt"
-          "rust-src"
-          "llvm-tools-preview"
-        ];
-      })
+  modules.home-manager = {
+    programs.cargo = {
+      enable = true;
 
-      libllvm
-      rust-analyzer
-      pkg-config
-      grcov
+      settings = {
+        cache = {
+          auto-clean-frequency = "7 days";
+        };
+      };
+    };
 
-      cargo-audit
-      cargo-binutils
-      cargo-bloat
-      cargo-mutants
-      cargo-msrv
-      cargo-udeps
-
-      tokio-console
-    ];
-
-    sessionVariables =
-      with pkgs;
-      lib.mkMerge [
-        (lib.mkIf isDarwin {
-          LIBRARY_PATH = "$LIBRARY_PATH\${LIBRARY_PATH:+:}" + "${darwin.libiconv}/lib";
+    home = {
+      packages = with pkgs; [
+        (rust-bin.stable.latest.minimal.override {
+          extensions = [
+            "clippy"
+            "rustfmt"
+            "rust-src"
+            "llvm-tools-preview"
+          ];
         })
+
+        libllvm
+        rust-analyzer
+        pkg-config
+        grcov
+
+        cargo-audit
+        cargo-binutils
+        cargo-bloat
+        cargo-mutants
+        cargo-msrv
+        cargo-udeps
+
+        tokio-console
       ];
+
+      sessionVariables =
+        with pkgs;
+        lib.mkMerge [
+          (lib.mkIf isDarwin {
+            LIBRARY_PATH = "$LIBRARY_PATH\${LIBRARY_PATH:+:}" + "${darwin.libiconv}/lib";
+          })
+        ];
+    };
   };
 }
