7e5e28fc5f5267e1dc5ba8fb7955ff646bb7a76b
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-12-12 08:00:14 +0100

    feat(gitlab): add linter
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/program/gitlab.nix
+++ profiles/program/gitlab.nix
@@ -6,6 +6,8 @@
       home.packages = with pkgs; [
         # 'glab config -g set check_update false' needs to be run after installation
         glab
+
+        gitlab-ci-linter
       ];
     };
   };
