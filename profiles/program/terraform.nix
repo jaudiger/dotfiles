3ae120de5d0180d9a902f7728a97ddbd47faa073
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-01-07 08:52:04 +0100

    chore(terraform): simplify the alias commands, and remove the unused ones
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/program/terraform.nix
+++ profiles/program/terraform.nix
@@ -18,18 +18,8 @@
     };
 
     host.shell.aliases = {
-      # Terraform
       tf = "terraform";
-      tfi = "terraform init";
-      tfp = "terraform plan";
-      tfa = "terraform apply";
-      tfd = "terraform destroy";
-
-      # Terragrunt
       tg = "terragrunt";
-      tgi = "terragrunt init --terragrunt-source-update --terragrunt-working-dir";
-      tga = "terragrunt apply --terragrunt-working-dir";
-      tgd = "terragrunt destroy --terragrunt-working-dir";
     };
   };
 }
