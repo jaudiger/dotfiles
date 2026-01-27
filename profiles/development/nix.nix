1059c2b68d3c346d223de338805a88d02caffa6b
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-01-27 15:20:35 +0100

    chore(nix): add nixd server settings
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/development/nix.nix
+++ profiles/development/nix.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  isDarwin = config.nixpkgs.hostPlatform.isDarwin;
+  host = config.modules.host;
+in
 {
   modules.home-manager = {
     home = {
@@ -21,6 +25,20 @@
       plugins.lsp.servers = {
         nixd = {
           enable = true;
+          settings = {
+            nixpkgs.expr = "import <nixpkgs> {}";
+            options =
+              if isDarwin then
+                {
+                  darwin.expr = ''(builtins.getFlake "${host.dotfilesDirectory}").darwinConfigurations.darwin-aarch64.options'';
+                  home-manager.expr = ''(builtins.getFlake "${host.dotfilesDirectory}").darwinConfigurations.darwin-aarch64.options.home-manager'';
+                }
+              else
+                {
+                  nixos.expr = ''(builtins.getFlake "${host.dotfilesDirectory}").nixosConfigurations.nixos-aarch64.options'';
+                  home-manager.expr = ''(builtins.getFlake "${host.dotfilesDirectory}").nixosConfigurations.nixos-aarch64.options.home-manager'';
+                };
+          };
         };
       };
     };
