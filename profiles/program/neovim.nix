6a06df09d60906c40f47b0d9ac508bf94e53a357
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-01-27 14:37:50 +0100

    fix(neovim): update fallback for folding
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/program/neovim.nix
+++ profiles/program/neovim.nix
@@ -168,7 +168,7 @@
             settings = {
               provider_selector = ''
                 function(bufnr, filetype, buftype)
-                  return { 'lsp', 'treesitter', 'indent' }
+                  return { 'treesitter', 'indent' }
                 end
               '';
             };
