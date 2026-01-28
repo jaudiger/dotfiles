9d66e27add4014b0e81187b8273b7704481ccb0f
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-01-28 17:07:35 +0100

    chore(android): setup pmbootstrap on Linux
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/toolchains/android.nix
+++ profiles/toolchains/android.nix
@@ -1,11 +1,24 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+let
+  isLinux = config.nixpkgs.hostPlatform.isLinux;
+in
 {
   modules.home-manager = {
     home = {
-      packages = with pkgs; [
-        android-tools
-      ];
+      packages =
+        with pkgs;
+        [
+          android-tools
+        ]
+        ++ lib.optionals isLinux [
+          pmbootstrap
+        ];
     };
   };
 }
