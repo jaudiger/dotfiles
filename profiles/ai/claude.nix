514ec353edc0d2300b6bbdcb07fd834a50d23755
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-03-07 19:40:11 +0100

    chore(claude-code): unset model in settings
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/ai/claude.nix
+++ profiles/ai/claude.nix
@@ -205,7 +205,6 @@ in
             CLAUDE_CODE_HIDE_ACCOUNT_INFO = 1;
             FORCE_AUTOUPDATE_PLUGINS = 1;
           };
-          model = "opusplan";
           permissions = {
             allow = [
               "Bash(cargo bench:*)"
