c71783491f4cc4d60083593f05c04070bdbe8756
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-03-18 09:40:48 +0100

    chore(c): enable Dockerfile LSP support in neovim
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/toolchains/dockerfile.nix
+++ profiles/toolchains/dockerfile.nix
@@ -1,10 +1,24 @@
 { pkgs, ... }:
 
 {
-  modules.home-manager.home = {
-    packages = with pkgs; [
-      # Dockerfile linter
-      hadolint
-    ];
+  modules.home-manager = {
+    home = {
+      packages = with pkgs; [
+        # Dockerfile linter
+        hadolint
+
+        # Language server
+        dockerfile-language-server
+      ];
+    };
+
+    # Neovim configuration
+    programs.nixvim = {
+      plugins.lsp.servers = {
+        dockerls = {
+          enable = true;
+        };
+      };
+    };
   };
 }
