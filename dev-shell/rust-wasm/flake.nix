ac5055c2093c210276b6776c495a631801871f59
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-04-17 08:52:28 +0200

    chore: updare Rust version in dev shells
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- dev-shell/rust-wasm/flake.nix
+++ dev-shell/rust-wasm/flake.nix
@@ -42,7 +42,7 @@
         {
           default = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
-              (rust-bin.stable."1.94.0".minimal.override {
+              (rust-bin.stable."1.95.0".minimal.override {
                 extensions = [
                   "rust-src"
                   "clippy"
