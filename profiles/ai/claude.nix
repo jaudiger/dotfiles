189ea5a45c5c6be9ccfadd6e87267d133f42c5e3
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-05-17 12:41:20 +0200

    chore(claude): update default model
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/ai/claude.nix
+++ profiles/ai/claude.nix
@@ -204,7 +204,6 @@ in
             CLAUDE_CODE_HIDE_ACCOUNT_INFO = 1;
             FORCE_AUTOUPDATE_PLUGINS = 1;
           };
-          model = "opusplan";
           permissions = {
             allow = [
               "Bash(cargo bench:*)"
