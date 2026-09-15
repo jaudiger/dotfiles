2a2b5a87d803b08ca2079904f76232549ca22f77
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-09-15 21:45:13 +0200

    chore(nodejs): prefer installation of PNPM through Home Manager
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- profiles/toolchains/nodejs.nix
+++ profiles/toolchains/nodejs.nix
@@ -2,23 +2,25 @@
 
 {
   modules.home-manager = {
-    programs.npm = {
-      enable = true;
+    programs = {
+      npm = {
+        enable = true;
 
-      # List of default settings: 'npm config ls -l'
-      settings = {
-        fund = false;
-        update-notifier = false;
+        # List of default settings: 'npm config ls -l'
+        settings = {
+          fund = false;
+          update-notifier = false;
+        };
+      };
+
+      pnpm = {
+        enable = true;
       };
     };
 
-    home = {
-      packages = with pkgs; [
-        pnpm
-
-        prettier
-        eslint
-      ];
-    };
+    home.packages = with pkgs; [
+      prettier
+      eslint
+    ];
   };
 }
